9ac034528adf6516031e79938e78a51ed4b0b31c
Indra <thelordduck22@gmail.com>
2021-04-05 15:25:38 +0200

    validara ok
    estructura validar correcta, falta poner los patters y validar los campos que haga falta

--- js/script.js
+++ js/script.js
@@ -2,15 +2,11 @@ class Model {
   constructor() {
 
   }
-
-  print(text){
-  	return text;
-  }
 }
 
 class View {
   constructor() {
-  	
+
   }
 }
 
@@ -22,5 +18,87 @@ class Controller {
 }
 
 const app = new Controller(new Model(), new View())
-var text = app.model.print("Hola");
-console.log(text);
+
+//Verificar que s'ha de posar com a minim
+// un checkbox del formulari
+//fet amb jquery
+$(document).ready(function(){
+    var checkboxes = $('.checkboxes');
+    checkboxes.change(function(){
+        if($('.checkboxes:checked').length>0) {
+            checkboxes.removeAttr('required');
+        } else {
+            checkboxes.attr('required', 'required');
+        }
+    });
+});
+
+
+//tendre que ordenarlo bien donde toque, en el modelo, vista o controlador, ya mirare
+var formulario, nom, cognom;
+function iniciar(){
+  var btn = document.getElementById("btn");
+  formulario = document.querySelector("form[name='miformulario']");
+  nom = document.getElementById('nom').getElementsByTagName('input');
+  cognom = document.getElementById('cognom').getElementsByTagName('input');
+  email = document.getElementById('email').getElementsByTagName('input');
+  telefon = document.getElementById('telefon').getElementsByTagName('input');
+  passwd = document.getElementById('passwd').getElementsByTagName('input');
+  dataNaix = document.getElementById('dataNaix').getElementsByTagName('input');
+  adreca = document.getElementById('adreca').getElementsByTagName('input');
+  poblacio = document.getElementById('poblacio').getElementsByTagName('input');
+  pais = document.getElementById('pais').getElementsByTagName('input');
+  //este no tiene input, hay que cambiarlo
+  codiPostal = document.getElementById('codiPostal').getElementsByTagName('select');
+  //para saber el valor en selectedIndex usar 
+  //--> 
+  //--> codiPostal[0][codiPostal[0].options.selectedIndex].value
+  //este tiene 3 inputs entonces tipusEsp[0], tipusEsp[1], tipusEsp[2] son los valores de los checkbox
+  tipusEsp = document.getElementById('tipusEsp').getElementsByTagName('input');
+  //no tiene inputs, ver como hay que hacerlo
+  codiBancari = document.getElementById('codiBancari').getElementsByTagName('input');
+  //no tiene inputs, hay que ver como lo hago
+  codiSucursal = document.getElementById('codiSucursal').getElementsByTagName('input');
+  numBanc = document.getElementById('numBanc').getElementsByTagName('input');
+
+  //missatges d'error personalitzats, que apareixen al costat de cada camp
+  nomSuggestMessage = document.getElementById('nomSuggestMessage');
+  cognomSuggestMessage = document.getElementById('cognomSuggestMessage');
+  
+  nom[0].addEventListener("input", myfunc.validacion_nom);
+  cognom[0].addEventListener("input", myfunc.validacion_cognom);
+}
+
+myfunc = function(){
+  var flag = true;
+  function validacion_nom(){
+    if(!nom[0].checkValidity()){
+      nomSuggestMessage.style.visibility = "visible";
+    } else {
+      nomSuggestMessage.style.visibility = "hidden";
+    }
+    updateRegisterButton();
+  }
+  function validacion_cognom(){
+    if(!cognom[0].checkValidity()){
+      cognomSuggestMessage.style.visibility = "visible";
+    } else {
+      cognomSuggestMessage.style.visibility = "hidden";
+    }
+    updateRegisterButton();
+  }
+  function updateRegisterButton(){
+    if(formulario.checkValidity()){
+      btn.hidden = false;
+    } else {
+      btn.hidden = true;
+    }
+  }
+  return{
+    validacion_nom:validacion_nom,
+    validacion_cognom:validacion_cognom,
+    updateRegisterButton:updateRegisterButton
+  }
+}();
+
+addEventListener("load", iniciar);
